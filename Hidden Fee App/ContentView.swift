314f2d74df49c72f47e78e4cb064887f5092aff5
ALIOUGNINGUE <gninguealiou102@gmail.com>
2026-07-02 15:04:33 -0500

    Initial Commit

---
+++ Hidden Fee App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hidden Fee App
+//
+//  Created by Apple on 7/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hidden Fee App/Hidden_Fee_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hidden_Fee_AppApp.swift
+//  Hidden Fee App
+//
+//  Created by Apple on 7/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Hidden_Fee_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
